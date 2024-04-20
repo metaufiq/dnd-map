8308c91721be21fb7c5553bb6cd984ee9c06a1e8
Muhammad Taufiqulsadi <mtaufiqulsadi@gmail.com>
2024-04-20 14:21:42 +0700

    add dashboard view

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,13 +1,19 @@
-import LoginView from '@/views/Login/LoginView.vue'
 import { createRouter, createWebHistory } from 'vue-router'
 import { PATH } from './constants'
+import DashboardView from '@/views/Dashboard/DashboardView.vue'
+import LoginView from '@/views/Login/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: PATH.HOME,
-      name: 'home',
+      name: 'dashboard',
+      component: DashboardView
+    },
+    {
+      path: PATH.LOGIN,
+      name: 'login',
       component: LoginView
     },
     {
---
+++ src/views/Dashboard/DashboardView.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="container">
+    <nav class="sidebar"></nav>
+    <div class="content"></div>
+  </div>
+</template>
+
+<style scoped>
+.container {
+  display: flex;
+  flex: 1;
+  flex-direction: row;
+  width: 100vw;
+  height: 100vh;
+}
+.sidebar {
+  flex: 1;
+  background-color: red;
+}
+.content {
+  flex: 3;
+  background-color: blue;
+}
+</style>
